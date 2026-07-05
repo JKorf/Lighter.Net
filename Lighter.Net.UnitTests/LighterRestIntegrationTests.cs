c8217d3fb264945b8d7c8a095b15ceeb0d6ab98d
JKorf <jankorf91@gmail.com>
2026-07-05 14:32:00 +0200

    Extended integration tests

--- Lighter.Net.UnitTests/LighterRestIntegrationTests.cs
+++ Lighter.Net.UnitTests/LighterRestIntegrationTests.cs
@@ -19,10 +19,22 @@ namespace Lighter.Net.UnitTests
 
         public override LighterRestClient GetClient(ILoggerFactory loggerFactory)
         {
+            string pk = Environment.GetEnvironmentVariable("PUBLICKEY");
+            int accountIndex = int.Parse(Environment.GetEnvironmentVariable("ACCOUNTINDEX") ?? "0");
+            int apiKeyIndex = int.Parse(Environment.GetEnvironmentVariable("APIKEYINDEX") ?? "0");
+            string sec = Environment.GetEnvironmentVariable("APISECRET");
+
+            Authenticated = pk != null && sec != null;
             return new LighterRestClient(null, loggerFactory, Options.Create(new LighterRestOptions
             {
                 AutoTimestamp = false,
                 OutputOriginalData = true,
+                ApiCredentials = pk == null ? null : new LighterCredentials(
+                    EthKey.FromPublicKey(pk),
+                    accountIndex: accountIndex,
+                    apiKeyIndex: apiKeyIndex,
+                    apiSecret: sec
+                    )
             }));
         }
 
@@ -47,6 +59,23 @@ namespace Lighter.Net.UnitTests
         }
 
         [Test]
+        public async Task TestAccount()
+        {
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetAccountsByL1AddressAsync(default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetNonceAsync(default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetAccountsAsync(default, default, default, default, CancellationToken.None), true, true, ignoreProperties: ["code"]);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetAccountLimitsAsync(default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetAccountMetadataAsync(default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetPnlAsync(Resolution.OneHour, default, default, default, default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetLiquidationsAsync(default, default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetFundingHistoryAsync(default, default, default, default, default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetDepositHistoryAsync(default, default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetTransferHistoryAsync(default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetWithdrawHistoryAsync(default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Account.GetApiKeysAsync(default, default, CancellationToken.None), true, true);
+        }
+
+        [Test]
         public async Task TestExchangeData()
         {
             await RunAndCheckResult(client => client.ExchangeApi.ExchangeData.GetStatusAsync(CancellationToken.None), false, true);
@@ -63,6 +92,15 @@ namespace Lighter.Net.UnitTests
             await RunAndCheckResult(client => client.ExchangeApi.ExchangeData.GetMarkPriceKlinesAsync("ETH", KlineInterval.OneDay, default, default, default, CancellationToken.None), false, true);
             await RunAndCheckResult(client => client.ExchangeApi.ExchangeData.GetFundingRateHistoryAsync("ETH", FundingResolution.OneDay, default, default, default, CancellationToken.None), false, true);
             await RunAndCheckResult(client => client.ExchangeApi.ExchangeData.GetFundingRatesAsync(CancellationToken.None), false, true);
+        }
+
+        [Test]
+        public async Task TestTrading()
+        {
+            await RunAndCheckResult(client => client.ExchangeApi.Trading.GetOpenOrdersAsync(default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Trading.GetClosedOrdersAsync(default, default, default, default, default, default, CancellationToken.None), true, true);
+            await RunAndCheckResult(client => client.ExchangeApi.Trading.GetUserTradesAsync(default, default, default, default, default, default, default, default, default, default, default, default, CancellationToken.None), true, true,
+                 ignoreProperties: ["trade_id_str", "ask_id_str", "bid_id_str", "ask_client_id_str", "bid_client_id_str"]);
         }
     }
 }
