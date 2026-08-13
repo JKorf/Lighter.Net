6edd9991292ae293aa879c7e8f998682a6a3ce8c
Jkorf <jankorf91@gmail.com>
2026-08-13 12:43:05 +0200

    Added Shared APIs Rest integration tests

--- Lighter.Net/Clients/ExchangeApi/LighterRestClientExchangeApiShared.cs
+++ Lighter.Net/Clients/ExchangeApi/LighterRestClientExchangeApiShared.cs
@@ -1684,7 +1684,7 @@ namespace Lighter.Net.Clients.ExchangeApi
 
         #region Open Interest client
 
-        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, true);
+        GetOpenInterestOptions IOpenInterestRestClient.GetOpenInterestOptions { get; } = new GetOpenInterestOptions(_exchangeName, false);
         async Task<HttpResult<SharedOpenInterest>> IOpenInterestRestClient.GetOpenInterestAsync(GetOpenInterestRequest request, CancellationToken ct)
         {
             var validationError = SharedClient.GetOpenInterestOptions.ValidateRequest(request, this);
