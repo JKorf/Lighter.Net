bdf426bd36f41d3c455458bdadcbe254284e20ab
Jkorf <jankorf91@gmail.com>
2026-09-15 15:11:47 +0200

    wip

--- Lighter.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ Lighter.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -32,29 +32,7 @@ namespace Microsoft.Extensions.DependencyInjection
             this IServiceCollection services,
             IConfiguration configuration)
         {
-            var options = new LighterOptions();
-            // Reset environment so we know if they're overridden
-            options.Rest.Environment = null!;
-            options.Socket.Environment = null!;
-            try
-            {
-                configuration.Bind(options);
-            }
-            catch (InvalidOperationException ex)
-            {
-                throw new InvalidOperationException("Invalid configuration provided", ex);
-            }
-
-            if (options.Rest == null || options.Socket == null)
-                throw new ArgumentException("Options null");
-
-            var restEnvName = options.Rest.Environment?.Name ?? options.Environment?.Name ?? LighterEnvironment.Live.Name;
-            var socketEnvName = options.Socket.Environment?.Name ?? options.Environment?.Name ?? LighterEnvironment.Live.Name;
-            options.Rest.Environment = LighterEnvironment.GetEnvironmentByName(restEnvName) ?? options.Rest.Environment!;
-            options.Rest.ApiCredentials = options.Rest.ApiCredentials ?? options.ApiCredentials;
-            options.Socket.Environment = LighterEnvironment.GetEnvironmentByName(socketEnvName) ?? options.Socket.Environment!;
-            options.Socket.ApiCredentials = options.Socket.ApiCredentials ?? options.ApiCredentials;
-
+            var options = LighterOptions.CreateFromConfiguration(configuration);
             services.AddSingleton(Options.Options.Create(options.Rest));
             services.AddSingleton(Options.Options.Create(options.Socket));
             services.AddSingleton(Options.Options.Create(options));
@@ -72,19 +50,7 @@ namespace Microsoft.Extensions.DependencyInjection
             this IServiceCollection services,
             Action<LighterOptions>? optionsDelegate = null)
         {
-            var options = new LighterOptions();
-            // Reset environment so we know if they're overridden
-            options.Rest.Environment = null!;
-            options.Socket.Environment = null!;
-            optionsDelegate?.Invoke(options);
-            if (options.Rest == null || options.Socket == null)
-                throw new ArgumentException("Options null");
-
-            options.Rest.Environment = options.Rest.Environment ?? options.Environment ?? LighterEnvironment.Live;
-            options.Rest.ApiCredentials = options.Rest.ApiCredentials ?? options.ApiCredentials;
-            options.Socket.Environment = options.Socket.Environment ?? options.Environment ?? LighterEnvironment.Live;
-            options.Socket.ApiCredentials = options.Socket.ApiCredentials ?? options.ApiCredentials;
-
+            var options = LighterOptions.Create(optionsDelegate);
             services.AddSingleton(Options.Options.Create(options.Rest));
             services.AddSingleton(Options.Options.Create(options.Socket));
             services.AddSingleton(Options.Options.Create(options));
--- Lighter.Net/Objects/Options/LighterOptions.cs
+++ Lighter.Net/Objects/Options/LighterOptions.cs
@@ -1,5 +1,7 @@
 using CryptoExchange.Net.Objects.Options;
 using CryptoExchange.Net.SharedApis;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace Lighter.Net.Objects.Options
 {
@@ -12,5 +14,64 @@ namespace Lighter.Net.Objects.Options
         /// Options for Shared API usage
         /// </summary>
         public SharedApiOptions SharedApi { get; set; } = new();
+        /// <summary>
+        /// Create LighterOptions instance using the provided configuration action
+        /// </summary>
+        public static LighterOptions Create(Action<LighterOptions>? configure = null)
+        {
+            var options = CreateUnconfigured();
+            configure?.Invoke(options);
+            return Normalize(options);
+        }
+
+        /// <summary>
+        /// Create LighterOptions using the provided IConfiguration
+        /// </summary>
+        public static LighterOptions CreateFromConfiguration(IConfiguration configuration)
+        {
+            if (configuration == null)
+                throw new ArgumentNullException(nameof(configuration));
+
+            var options = CreateUnconfigured();
+            try
+            {
+                configuration.Bind(options);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InvalidOperationException("Invalid Lighter configuration provided", ex);
+            }
+
+            if (options.Environment != null)
+                options.Environment = LighterEnvironment.GetEnvironmentByName(options.Environment.Name) ?? options.Environment;
+            if (options.Rest?.Environment != null)
+                options.Rest.Environment = LighterEnvironment.GetEnvironmentByName(options.Rest.Environment.Name) ?? options.Rest.Environment;
+            if (options.Socket?.Environment != null)
+                options.Socket.Environment = LighterEnvironment.GetEnvironmentByName(options.Socket.Environment.Name) ?? options.Socket.Environment;
+
+            return Normalize(options);
+        }
+
+        private static LighterOptions CreateUnconfigured()
+        {
+            var options = new LighterOptions();
+            options.Rest.Environment = null!;
+            options.Socket.Environment = null!;
+            return options;
+        }
+
+        private static LighterOptions Normalize(LighterOptions options)
+        {
+            if (options.Rest == null)
+                throw new ArgumentException("REST options cannot be null", nameof(options));
+            if (options.Socket == null)
+                throw new ArgumentException("Socket options cannot be null", nameof(options));
+
+            options.Rest.Environment ??= options.Environment ?? LighterEnvironment.Live;
+            options.Rest.ApiCredentials ??= options.ApiCredentials;
+            options.Socket.Environment ??= options.Environment ?? LighterEnvironment.Live;
+            options.Socket.ApiCredentials ??= options.ApiCredentials;
+            return options;
+        }
     }
 }
